44d5a709a79dc7a260f4b5ecef15eb9eee9efeae
Mehdi ZeroOne <mohammadmehdidiprostami14@gmail.com>
2026-09-01 11:05:10 +0330

    clear screen, draw, swap and events

--- src/main.cpp
+++ src/main.cpp
@@ -119,6 +119,16 @@ int main() {
     // Main loop
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
     }
     return 0;
 }
